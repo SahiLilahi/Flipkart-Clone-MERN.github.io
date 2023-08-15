94a560c85dd5f83c8f45b4ab75b9e17fcd3a4083
Sahil khan <65327070+SahiLilahi@users.noreply.github.com>
2023-08-15 23:05:10 +0530

    heyy

--- server/Routes/Route.js
+++ server/Routes/Route.js
@@ -1,10 +1,16 @@
 import express from "express";
-
+import {
+  getProductById,
+  getProducts,
+} from "../Controller/ProductController.js";
 import { userSignUp, userLogIn } from "../Controller/UserController.js";
 
 const router = express.Router();
 
 router.post("/signup", userSignUp);
 router.post("/login", userLogIn);
+
+router.get("/products", getProducts);
+router.get("/product/:id", getProductById);
 
 export default router;
